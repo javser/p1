5110729607fb5a6fc375949a284e08b9c4f2ce57
Serg <34738549+javser@users.noreply.github.com>
2020-02-09 23:12:22 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
 const container = document.querySelector(".container")
 const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
+  { name: "Perspiciatis", image: "https://media.giphy.com/media/3o6ZtlGkjeschymLNm/source.gif" },
 ]
 const showCoffees = () => {
   let output = ""
@@ -31,7 +23,7 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
       .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
+      .then(res => alert("service worker registered"))
+      .catch(err => alert("service worker not registered", err))
   })
 }
